dedbb338eca743a05de3af561f2397f45babf5b6
ruhi <rdoshi99@berkeley.edu>
2019-06-10 11:03:30 -0700

    working histogram colors, building off of the compressCamfiberPlots branch instead of master to add features

--- py/qqa/plots/fiber.py
+++ py/qqa/plots/fiber.py
@@ -1,110 +1,96 @@
 import numpy as np
-
-from astropy.table import Table, join
 import jinja2
+import desimodel.io
 
 import bokeh
 import bokeh.plotting as bk
 import bokeh.models
 from bokeh.embed import components
+
+from astropy.table import Table, join
 # from bokeh.models.tickers import FixedTicker
 # from bokeh.models.ranges import FactorRange
-
-from bokeh.models import LinearColorMapper, ColorBar # HoverTool, CustomJS, HTMLTemplateFormatter, NumeralTickFormatter
+from bokeh.models import LinearColorMapper, ColorBar, HoverTool #, CustomJS, HTMLTemplateFormatter
+from bokeh.models import ColumnDataSource, CDSView, BooleanFilter
 from bokeh.transform import transform
+from bokeh.transform import linear_cmap
+import bokeh.palettes as bp
+from bokeh.models import ColorBar, BasicTicker, NumeralTickFormatter
 
-import bokeh.palettes
 
-import desimodel.io
+from ..plots.core import get_colors, plot_histogram
 
-def get_colors(x, xmin=None, xmax=None):
+
+
+def plot_fibers(source, name, cam=None, width=250, height=270, zmin=None,
+                zmax=None, percentile=None, title=None, hist_x_range=None,
+                plate_x_range=None, plate_y_range=None, colorbar=False,
+                plot_hist=True, tools='box_select,reset', tooltips=None):
     '''
-    TODO: document
-    TODO: move this into plots.core or similar
-    '''
-    palette = np.array(bokeh.palettes.all_palettes['RdBu'][11])
-    n = len(palette)
-    
-    x = np.asarray(x)
-    if xmin is None:
-        xmin = np.min(x)
-    if xmax is None:
-        xmax = np.max(x)
-
-    x.clip(xmin, xmax)
-    ii = (n*(x-xmin) / (xmax-xmin)).astype(int).clip(0,n-1)
-    return palette[ii]
-
-def plot_fibers(qadata, name, cam=None, width=250, height=270,
-    zmin=None, zmax=None, percentile=None, title=None):
-    '''TODO: document
     ARGS:
-        qadata :  
-            #- TODO: would it be possible to just make this argument
-            #- an astropy table?
+        source :  ColumnDataSource object
         name : a string data column in qadata
-    
+
     Options:
         cam : string ('B', 'R', 'Z') to specify which camera wavelength
         (zmin,zmax) : hardcoded (min,max) to clip data
         percentile : (min,max) percentiles to clip data
         width, height : width and height of graph in pixels
         title : title for the plot
+        tools : string of supported features for the plot
+        tooltips : hovertool info
+        hist_x_range, plate_x_range, plate_y_range : figure ranges to support linking
 
-    
+
     Generates a focal plane plot with data per fiber color-coded based on its value
     Generates a histogram of NAME values per fiber
     '''
+    full_metric = np.array(source.data.get(name), copy=True)
+    #- TODO: add customizable clipping (percentiles, zmins, zmaxs)
 
-    #- bytes vs. str, what a pain
-    qadata = Table(qadata)
-    qadata['CAM'] = qadata['CAM'].astype(str)
-    
-    #- Filter data to just this camera
-    if cam is not None:
-        ii = (qadata['CAM'] == cam.lower()) | (qadata['CAM'] == cam.upper())
-        qadata = qadata[ii]
+    #- adjusts for outliers on the full scale
+    pmin_full, pmax_full = np.percentile(full_metric, (0, 95))
+    #full_metric = np.clip(full_metric, pmin_full, pmax_full)
+
+    #- Generate colors for both plots
+    palette = np.array(bp.RdYlBu[11])
+    mapper = linear_cmap(name, palette, low=pmin_full, high=pmax_full, nan_color='gray')
 
     #- Focal plane colored scatter plot
-    fig = bk.figure(width=width, height=height, toolbar_location=None, title=title)
-    
-    fiberpos = Table(desimodel.io.load_fiberpos())
-    fiberpos.remove_column('SPECTRO')
+    fig = bk.figure(width=width, height=height, title=title, tools=tools,
+                    x_range=plate_x_range, y_range=plate_y_range)
 
-    if len(qadata) > 0:
-        #- Join with fiberpos to know where fibers are on focal plane
-        #- TODO: use input fibermap instead
-        qadata = join(qadata, fiberpos, keys='FIBER')
+    #- Filter data to just this camera
+    #- TODO: DOES THIS WORK WITHOUT A CAM ARGUMENT PASSED IN?
+    booleans_metric = np.char.upper(np.array(source.data['CAM']).astype(str)) == cam.upper()
+    view_metric = CDSView(source=source, filters=[BooleanFilter(booleans_metric)])
+        #- TODO: switch to group filter
 
-        metric = np.array(qadata[name], copy=True)
-        if percentile is not None:
-            pmin, pmax = np.percentile(metric, percentile)
-            metric = np.clip(metric, pmin, pmax)
+    #- Plot only the fibers which measured the metric
+    s = fig.scatter('X', 'Y', source=source, view=view_metric, color=mapper,
+                    radius=5, alpha=0.7)#, hover_color='firebrick')
 
-        if zmin is not None or zmax is not None:
-            metric = np.clip(metric, zmin, zmax)
 
-        colors = get_colors(metric)
-        fig.scatter(qadata['X'], qadata['Y'], color=colors, radius=5, alpha=0.7)
+    #- Plot the rest of the fibers
+    fibers_measured = source.data['FIBER'][booleans_metric]
+    ii = ~np.in1d(source.data['FIBER'], fibers_measured)
+    booleans_empty = [fiber in ii for fiber in range(len(source.data))]
+    view_empty = CDSView(source=source, filters=[BooleanFilter(booleans_empty)])
+    fig.scatter('X', 'Y', source=source, view=view_empty, fill_color='#DDDDDD', radius=2)
 
-    ii = ~np.in1d(fiberpos['FIBER'], qadata['FIBER'])
-    fig.scatter(fiberpos['X'][ii], fiberpos['Y'][ii], color='#DDDDDD', radius=2)
-    
-    fig.x_range = bokeh.models.Range1d(-420, 420)
-    fig.y_range = bokeh.models.Range1d(-420, 420)
-    
+
+    #- Aesthetics: outline the focal plates by camera color, label cameras,
+    #- style visual attributes of the figure
     camcolors = dict(B='steelblue', R='firebrick', Z='gray')
-    
-    if cam is not None:
+
+    if cam:
         color = camcolors[cam.upper()]
         fig.ellipse(x=[0,], y=[0,], width=830, height=830, fill_color=None,
                     line_color=color, line_alpha=0.5, line_width=2)
-    
-    if cam is not None:
         fig.text([-350,], [350,], [cam.upper(),],
             text_color=camcolors[cam.upper()],
             text_align='center', text_baseline='middle')
-    
+
     fig.xgrid.grid_line_color = None
     fig.ygrid.grid_line_color = None
     fig.xaxis.major_tick_line_color = None
@@ -117,27 +103,32 @@ def plot_fibers(qadata, name, cam=None, width=250, height=270,
     fig.xaxis.major_label_text_font_size = '0pt'
     fig.yaxis.major_label_text_font_size = '0pt'
 
+    if colorbar:
+        fig.plot_width = fig.plot_width + 60
+        colorbar_offset = 12
+        color_bar = ColorBar(color_mapper=mapper['transform'], label_standoff=colorbar_offset,
+                border_line_color=None, location=(0,0), ticker=BasicTicker(), width=10,
+                formatter=NumeralTickFormatter(format='0.0a'))
+        fig.add_layout(color_bar, 'right')
+        #- adjusting histogram width for colorbar
+        width = fig.plot_width - 20 - colorbar_offset
+
+
+    if not plot_hist:
+        return fig, None
+
     #- Histogram of values
-    #- TODO: move basic histogram code into plots.core or similar
+    metric = full_metric[booleans_metric]
 
-    hfig = bk.figure(width=width, height=80)
+    if any(booleans_metric):
+        if percentile:
+            pmin, pmax = np.percentile(metric, percentile)
+            metric = np.clip(metric, pmin, pmax)
+        if zmin or zmax:
+            metric = np.clip(metric, zmin, zmax)
 
-    if len(qadata) > 0:
-        hist, edges = np.histogram(metric, density=True, bins=50)
-        centers = 0.5*(edges[1:] + edges[:-1])
-        colors = get_colors(centers)
-        hfig.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:], color=colors, alpha=0.5)
-    
+    hfig = plot_histogram(metric, title=name, width=width, x_range=hist_x_range, num_bins=50,
+                         palette=mapper['transform'].palette, low=pmin_full, high=pmax_full)
 
-    hfig.xaxis.axis_label = name
-    hfig.toolbar_location = None
-    hfig.title.text_color = '#ffffff'
-    hfig.yaxis.major_label_text_font_size = '0pt'
-    hfig.xgrid.grid_line_color = None
-    hfig.ygrid.grid_line_color = None
-    hfig.yaxis.major_tick_line_color = None
-    hfig.yaxis.minor_tick_line_color = None
-    hfig.outline_line_color = None
-    hfig.yaxis.axis_line_color = None
 
     return fig, hfig
--- py/qqa/webpages/camfiber.py
+++ py/qqa/webpages/camfiber.py
@@ -8,10 +8,13 @@ from bokeh.embed import components
 
 from ..plots.fiber import plot_fibers
 from ..plots.camfiber import plot_per_camfiber
+import desimodel.io
+from bokeh.models import ColumnDataSource
+from astropy.table import Table, join
 
 def write_camfiber_html(outfile, data, header):
     '''TODO: document'''
-    
+
     night = header['NIGHT']
     expid = header['EXPID']
     flavor = header['FLAVOR'].rstrip()
@@ -32,31 +35,70 @@ def write_camfiber_html(outfile, data, header):
         flavor=flavor, program=program, qatype = 'camfiber',
     )
 
-    #- TODO: refactor these to reduce replicated code, while still supporting
-    #- customizations like percentile vs. zmin/zmax
+    #- List of attributes to plot per camfiber with default arguments
+    ATTRIBUTES = ['INTEG_RAW_FLUX', 'MEDIAN_RAW_FLUX', 'MEDIAN_RAW_SNR', 'INTEG_CALIB_FLUX',
+                 'MEDIAN_CALIB_FLUX', 'MEDIAN_CALIB_SNR']
 
     #- Default cameras and percentile ranges for camfiber plots
     CAMERAS = ['B', 'R', 'Z']
     PERCENTILES = {'B':(0, 95), 'R':(0, 95), 'Z':(0, 98)}
-    #- List of attributes to plot per camfiber with default arguments
-    ATTRIBUTES = ['INTEG_RAW_FLUX', 'MEDIAN_RAW_FLUX', 'MEDIAN_RAW_SNR', 'INTEG_CALIB_FLUX',
-                 'MEDIAN_CALIB_FLUX', 'MEDIAN_CALIB_SNR']
-    TITLES = {'INTEG_RAW_FLUX':'Integrated Raw Counts', 'MEDIAN_RAW_FLUX':'Median Raw Counts', 
+    TITLES = {'INTEG_RAW_FLUX':'Integrated Raw Counts', 'MEDIAN_RAW_FLUX':'Median Raw Counts',
               'MEDIAN_RAW_SNR':'Median S/N', 'INTEG_CALIB_FLUX':'Integrated Calibration Flux',
               'MEDIAN_CALIB_FLUX':'Median Calibration Flux', 'MEDIAN_CALIB_SNR':
               'Median Calibration S/N'}
     TITLESPERCAM = {'B':TITLES}
-    
+    TOOLS = 'box_select,reset'
+
+
+    #- Gets a shared ColumnDataSource of DATA
+    cds = create_cds(data, ATTRIBUTES)
+
+
+    #- Gets the html components for each camfib plot in ATTRIBUTES
     for attr in ATTRIBUTES:
-        plot_per_camfiber(data, attr, CAMERAS, html_components, percentiles=PERCENTILES, 
-            titles=TITLESPERCAM)
-            
+        plot_per_camfiber(cds, attr, CAMERAS, html_components, percentiles=PERCENTILES,
+            titles=TITLESPERCAM, tools=TOOLS)
+
     #- Combine template + components -> HTML
     html = template.render(**html_components)
-    
+
     #- Write HTML text to the output file
     with open(outfile, 'w') as fx:
         fx.write(html)
 
     return html_components
-    
+
+
+def create_cds(data, attributes):
+    '''
+    Creates a ColumnDataSource object from DATA
+    Args:
+        data : a fits file of camfib data collected
+        attributes : a list of metrics
+
+    Returns a bokeh ColumnDataSource object
+    '''
+    #- Get the positions of the fibers on the focal plane
+    fiberpos = Table(desimodel.io.load_fiberpos())
+    fiberpos.remove_column('SPECTRO')
+
+    #- Join the metrics data with the corresponding fibers
+    #- TODO: use input fibermap instead
+    data = Table(data)
+    if len(data) > 0:
+        data = join(data, fiberpos, keys='FIBER')
+
+        #- bytes vs. strings
+        for colname in data.colnames:
+            if data[colname].dtype.kind == 'S':
+                data[colname] = data[colname].astype(str)
+
+    data_dict = dict({})
+    for colname in data.dtype.names:
+        if colname in attributes:
+            data_dict[colname] = data[colname].astype(np.float32)
+        else:
+            data_dict[colname] = data[colname]
+    cds = ColumnDataSource(data=data_dict)
+
+    return cds
