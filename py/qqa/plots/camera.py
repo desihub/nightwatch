d8f69ee190a0e6f954b3677e52ac5a27c5c11ebe
Stephen Bailey <stephen.code@gmail.com>
2019-06-11 22:59:41 -0700

    standardize y ranges

--- py/qqa/plots/camera.py
+++ py/qqa/plots/camera.py
@@ -1,10 +1,11 @@
+import numpy as np
 import bokeh.plotting as bk
 from bokeh.models import ColumnDataSource, Whisker
 from bokeh.layouts import column
 from bokeh.models.tickers import FixedTicker
 from astropy.table import Table
 
-def plot_camera_qa(table, attribute, height=225, width=450, title=None, line0 = True):
+def plot_camera_qa(table, attribute, height=225, width=450, title=None, line0 = True, minmax=None):
     astrotable = Table(table)
     if title is None:
         title=attribute
@@ -29,7 +30,28 @@ def plot_camera_qa(table, attribute, height=225, width=450, title=None, line0 = 
             Whisker(source=source, base="SPECTRO", upper="MAXattr", lower="MINattr")
         )
         fig.xaxis.ticker = FixedTicker(ticks=[i for i in range(0, 10, 1)])
-        fig.xaxis.axis_label = "Spectrograph number"
+        if cam == 'Z':
+            fig.xaxis.axis_label = "Spectrograph number"
         fig.yaxis.axis_label = attribute
+        if minmax is not None:
+            ymin, ymax = minmax
+            attrmin = np.min(cam_table["MIN"+attribute])
+            attrmax = np.max(cam_table["MAX"+attribute])
+
+            #- Data ranges are within minmax
+            if ymin < attrmin and ymax > attrmax:
+                fig.y_range.start = ymin
+                fig.y_range.end = ymax
+            #- Otherwise default to using data range
+            else:
+                from bokeh.models import BoxAnnotation
+                fig.add_layout(BoxAnnotation(
+                    bottom=ymin, top=ymax,
+                    fill_color='green', fill_alpha=0.1)
+                    )
+                tmp = max(abs(attrmin), abs(attrmax))
+                fig.y_range.start = -tmp * 1.1
+                fig.y_range.end = tmp * 1.1
+
         cam_figs += [fig]
     return column(cam_figs)
--- py/qqa/webpages/camera.py
+++ py/qqa/webpages/camera.py
@@ -30,21 +30,25 @@ def write_camera_html(outfile, data, header):
     )
 
     #- Generate the bokeh figures
-    fig = plot_camera_qa(data, 'DX', title='DX with camera')
+    fig = plot_camera_qa(data, 'DX', title='DX with camera', minmax=(-0.1, 0.1))
     script, div = components(fig)
     html_components['DX'] = dict(script=script, div=div)
 
-    fig = plot_camera_qa(data, 'DY', title='DY with camera')
+    fig = plot_camera_qa(data, 'DY', title='DY with camera', minmax=(-0.1, 0.1))
     script, div = components(fig)
     html_components['DY'] = dict(script=script, div=div)
 
-    fig = plot_camera_qa(data, 'XSIG', title='XSIG with camera', line0=False)
-    script, div = components(fig)
-    html_components['XSIG'] = dict(script=script, div=div)
+    if "MEANXSIG" in data.dtype.names:
+        fig = plot_camera_qa(data, 'XSIG', title='XSIG with camera',
+                line0=False, minmax=(0.8, 1.3))
+        script, div = components(fig)
+        html_components['XSIG'] = dict(script=script, div=div)
 
-    fig = plot_camera_qa(data, 'YSIG', title='YSIG with camera', line0=False)
-    script, div = components(fig)
-    html_components['YSIG'] = dict(script=script, div=div)
+    if "MEANYSIG" in data.dtype.names:
+        fig = plot_camera_qa(data, 'YSIG', title='YSIG with camera',
+                line0=False, minmax=(0.8, 1.3))
+        script, div = components(fig)
+        html_components['YSIG'] = dict(script=script, div=div)
 
     #- Combine template + components -> HTML
     html = template.render(**html_components)
