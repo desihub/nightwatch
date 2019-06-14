463432e901fb9fa159beb063b62b56131137d558
Anasuya Lyons <anasuya.lyons@gmail.com>
2019-06-14 16:56:18 -0700

    added green bands for acceptable range

--- py/qqa/plots/amp.py
+++ py/qqa/plots/amp.py
@@ -4,9 +4,11 @@ import bokeh
 import bokeh.plotting as bk
 from bokeh.models.tickers import FixedTicker
 from bokeh.models.ranges import FactorRange
-from bokeh.models import LinearColorMapper, ColorBar, ColumnDataSource, OpenURL, TapTool, Div, HoverTool
+from bokeh.models import LinearColorMapper, ColorBar, ColumnDataSource, OpenURL, TapTool, Div, HoverTool, Range1d, BoxAnnotation
 import bokeh.palettes
 from bokeh.layouts import column, gridplot
+
+import json
 
 def get_amp_colors(data, threshold):
     '''takes in per amplifier data and the acceptable threshold for that metric (TO DO: update this once
@@ -52,83 +54,93 @@ def isolate_spec_lines(data_locs, data):
         data_groups.append(data[ids[i]:ids[i+1]])
     return spec_groups, data_groups
 
-def plot_amp_qa(data, name, title=None, palette="YlGn9", qamin=None, qamax=None, plot_height=80, 
-                plot_width=700):
+def plot_amp_cam_qa(data, name, cam, labels, qamin, qamax, title=None, palette="YlGn9", plot_height=80, plot_width=700):
     '''Plot a per-amp visualization of data[name]
     qamin/qamax: min/max ranges for the color scale'''
     
     if title is None:
-        title = name
+        title = name   
+    
+    #unpacking data
+    spec_loc = []
+    amp_loc = []
+    name_data = []
+    for row in data:
+        if row['CAM'] in (cam, cam.encode('utf-8')):
+            amp_loc.append(row['AMP'].decode('utf-8'))
+            spec_loc.append(str(row['SPECTRO']))
+            name_data.append(row[name])
+        else:
+            continue
+    _, ids = np.unique(spec_loc, return_index=True)
+    spec_loc = np.array(spec_loc)[np.sort(ids)]
+    _, ids = np.unique(amp_loc, return_index=True)
+    amp_loc = np.array(amp_loc)[np.sort(ids)]
+    
+    locations = [(spec, amp) for spec in spec_loc for amp in amp_loc]
+    
+    colors = get_amp_colors(name_data, qamax)
+    sizes = get_amp_size(name_data, qamax)
+   
+    source = ColumnDataSource(data=dict(
+        name_data=name_data,
+        locations=locations,
+        colors=colors,
+        sizes=sizes,
+    ))
+    
+    axis = bk.figure(x_range=FactorRange(*labels), toolbar_location=None, 
+                     plot_height=50, plot_width=plot_width,
+                     y_axis_location=None)      
+    #plotting
+    hover= HoverTool(tooltips = [
+        ('(spec, amp)', '@locations'),
+        ('{}'.format(name), '@name_data')],
+                      line_policy='nearest')
+    
+    fig = bk.figure(x_range=axis.x_range, 
+                      toolbar_location=None, plot_height=plot_height, 
+                      plot_width=plot_width, x_axis_location=None, tools=[hover])
+    
+    spec_groups, data_groups = isolate_spec_lines(locations, name_data)
+    for i in range(len(spec_groups)):
+        fig.line(x=spec_groups[i], y=data_groups[i], line_color='black', alpha=0.25)
+        
+    fig.circle(x='locations', y='name_data', line_color=None, 
+                 fill_color='colors', size='sizes', source=source)
+
+    fig.yaxis.axis_label = cam
+    fig.y_range = Range1d(np.min(name_data)*0.9, np.max(name_data)*1.1)
+    fig.yaxis.minor_tick_line_color=None
+    fig.ygrid.grid_line_color=None
+    if cam == 'R':
+        fig.outline_line_color='firebrick'
+    if cam == 'B':
+        fig.outline_line_color='steelblue'
+        fig.title.text = title
+    if cam =='Z':
+        fig.outline_line_color='grey'
+    fig.outline_line_alpha=0.7
+    
+    good_range = BoxAnnotation(bottom=qamin, top=qamax, fill_alpha=0.1, fill_color='green')
+    fig.add_layout(good_range)
+
+    return fig
+
+def plot_amp_qa(data, name, title=None, palette="YlGn9", qamin=None, qamax=None, plot_height=80, 
+                plot_width=700):
+    
     if qamin is None:
         qamin = np.min(data[name])
     if qamax is None:
-        qamax = np.max(data[name])    
+        qamax = np.max(data[name]) 
     
-    #unpacking data
-    spec_loc_R = []
-    amp_loc_R = []
-    spec_loc_B = []
-    amp_loc_B = []
-    spec_loc_Z = []
-    amp_loc_Z = []
-    data_R = []
-    data_B = []
-    data_Z = []
-    for row in data:
-        if row['CAM'] in ('R', b'R'):
-            amp_loc_R.append(row['AMP'].decode('utf-8'))
-            spec_loc_R.append(str(row['SPECTRO']))
-            data_R.append(row[name])
-        if row['CAM'] in ('B', b'B'):
-            amp_loc_B.append(row['AMP'].decode('utf-8'))
-            spec_loc_B.append(str(row['SPECTRO']))
-            data_B.append(row[name])
-        if row['CAM'] in ('Z', b'Z'):
-            amp_loc_Z.append(row['AMP'].decode('utf-8'))
-            spec_loc_Z.append(str(row['SPECTRO']))
-            data_Z.append(row[name])
-    
-    spec_loc, ids = np.unique(spec_loc_R, return_index=True)
-    spec_loc_R = np.array(spec_loc_R)[np.sort(ids)]
-    amp_loc, ids = np.unique(amp_loc_R, return_index=True)
-    amp_loc_R = np.array(amp_loc_R)[np.sort(ids)]
-    spec_loc, ids = np.unique(spec_loc_B, return_index=True)
-    spec_loc_B = np.array(spec_loc_B)[np.sort(ids)]
-    amp_loc, ids = np.unique(amp_loc_B, return_index=True)
-    amp_loc_B = np.array(amp_loc_B)[np.sort(ids)]
-    spec_loc, ids = np.unique(spec_loc_Z, return_index=True)
-    spec_loc_Z = np.array(spec_loc_Z)[np.sort(ids)]
-    amp_loc, ids = np.unique(amp_loc_Z, return_index=True)
-    amp_loc_Z = np.array(amp_loc_Z)[np.sort(ids)]
-    
-    locations_R = [(spec, amp) for spec in spec_loc_R for amp in amp_loc_R]
-    locations_B = [(spec, amp) for spec in spec_loc_B for amp in amp_loc_B]
-    locations_Z = [(spec, amp) for spec in spec_loc_Z for amp in amp_loc_Z]
-    
-    colors_Z = get_amp_colors(data_Z, qamax)
-    colors_R = get_amp_colors(data_R, qamax)
-    colors_B = get_amp_colors(data_B, qamax)
-    sizes_Z = get_amp_size(data_Z, qamax)
-    sizes_R = get_amp_size(data_R, qamax)
-    sizes_B = get_amp_size(data_B, qamax)
-   
-    source = ColumnDataSource(data=dict(
-        data_R=data_R,
-        data_B=data_B,
-        data_Z=data_Z,
-        locations_R=locations_R,
-        locations_B = locations_B,
-        locations_Z = locations_Z,
-        colors_Z=colors_Z,
-        colors_B=colors_B,
-        colors_R=colors_R,
-        sizes_Z=sizes_Z,
-        sizes_B=sizes_B,
-        sizes_R=sizes_R,
-    ))
+    labels = [(spec, amp) for spec in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] for amp in ['A', 'B', 'C', 'D']]
+    fig_B = plot_amp_cam_qa(data, name, 'B', labels, qamin, qamax, title=title, palette=palette, plot_height=plot_height)
+    fig_R = plot_amp_cam_qa(data, name, 'R', labels, qamin, qamax, title=title, palette=palette, plot_height=plot_height)
+    fig_Z = plot_amp_cam_qa(data, name, 'Z', labels, qamin, qamax, title=title, palette=palette, plot_height=plot_height)
     
     #x-axis
-    labels = [(spec, amp) for spec in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] for amp in ['A', 'B', 'C', 'D']]
     axis = bk.figure(x_range=FactorRange(*labels), toolbar_location=None, 
                      plot_height=50, plot_width=plot_width,
                      y_axis_location=None)         
@@ -136,70 +148,8 @@ def plot_amp_qa(data, name, title=None, palette="YlGn9", qamin=None, qamax=None,
     axis.grid.grid_line_color=None
     axis.outline_line_color=None
     
-    #R
-    hover_R= HoverTool(tooltips = [
-        ('(spec, amp)', '@locations_R'),
-        ('{}'.format(name), '$y')],
-                      line_policy='next')
-    fig_R = bk.figure(x_range=axis.x_range, 
-                      toolbar_location=None, plot_height=plot_height, 
-                      plot_width=plot_width, x_axis_location=None, tools=[hover_R])
-    
-    spec_groups, data_groups = isolate_spec_lines(locations_R, data_R)
-    for i in range(len(spec_groups)):
-        fig_R.line(x=spec_groups[i], y=data_groups[i], line_color='black', alpha=0.25)
-        
-    fig_R.circle(x='locations_R', y='data_R', line_color=None, 
-                 fill_color='colors_R', size='sizes_R', source=source)
-
-    fig_R.yaxis.axis_label = 'R'
-    fig_R.yaxis.minor_tick_line_color=None
-    fig_R.ygrid.grid_line_color=None
-    fig_R.outline_line_color='firebrick'
-    fig_R.outline_line_alpha=0.7
-
-    #B
-    hover_B= HoverTool(tooltips = [
-        ('(spec, amp)', '@locations_B'),
-        ('{}'.format(name), '$y')],
-                      line_policy='next')
-    fig_B = bk.figure(x_range=axis.x_range, toolbar_location=None, x_axis_location=None,
-                   plot_height=plot_height+25, plot_width=plot_width, title=title, tools=[hover_B])
-        
-    spec_groups, data_groups = isolate_spec_lines(locations_B, data_B)
-    for i in range(len(spec_groups)):
-        fig_B.line(x=spec_groups[i], y=data_groups[i], line_color='black', alpha=0.25)
-    
-    circle_B = fig_B.circle(x='locations_B', y='data_B', line_color=None, 
-                 fill_color='colors_B', size='sizes_B', source=source)
-        
-    fig_B.yaxis.axis_label = 'B'
-    fig_B.ygrid.grid_line_color=None
-    fig_B.yaxis.minor_tick_line_color=None
-    fig_B.outline_line_color='steelblue'
-    fig_B.outline_line_alpha=0.7
-
-    #Z
-    hover_Z= HoverTool(tooltips = [
-        ('(spec, amp)', '@locations_Z'),
-        ('{}'.format(name), '$y')],
-                      line_policy='next')
-    fig_Z = bk.figure(x_range=axis.x_range, toolbar_location=None, x_axis_location=None,
-                   plot_height=plot_height, plot_width=plot_width, tools=[hover_Z])
-        
-    spec_groups, data_groups = isolate_spec_lines(locations_Z, data_Z)
-    for i in range(len(spec_groups)):
-        fig_Z.line(x=spec_groups[i], y=data_groups[i], line_color='black', alpha=0.25)
-    
-    circle_Z = fig_Z.circle(x='locations_Z', y='data_Z', line_color=None, 
-                 fill_color='colors_Z', size='sizes_Z', source=source)
-    
-    fig_Z.yaxis.axis_label = 'Z'
-    fig_Z.ygrid.grid_line_color=None
-    fig_Z.yaxis.minor_tick_line_color=None
-    fig_Z.outline_line_color='grey'
-    fig_Z.outline_line_alpha=0.7
-
     fig = gridplot([[fig_B], [fig_R], [fig_Z], [axis]], toolbar_location=None)
     
     return fig
+        
+        
