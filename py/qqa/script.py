2026da7ca9d50a5de849fcc0647e4a63f32c0d63
Ruhi Doshi <rdoshi99@cori12.nersc.gov>
2019-06-24 13:11:12 -0700

    makes batch processing an option

--- py/qqa/script.py
+++ py/qqa/script.py
@@ -62,7 +62,7 @@ def main_monitor(options=None):
     parser.add_argument("--cameras", type=str, help="comma separated list of cameras (for debugging)")
     parser.add_argument("--catchup", action="store_true", help="Catch up on processing all unprocessed data")
     parser.add_argument("--waittime", type=int, default=5, help="Seconds to wait between checks for new data")
-    parser.add_argument("--batch", "-b", type=bool, default=True, help="True if you want qproc data processing to spawn a batch job")
+    parser.add_argument("--batch", "-b", type=bool, default=False, help="True if you want qproc data processing to spawn a batch job")
     parser.add_argument("--nodes", "-N", type=int, default=1, help="Number of nodes for qproc batch job")
     parser.add_argument("--ntasks", "-n", type=int, default=1, help="Number of tasks per node for qproc batch job")
     parser.add_argument("--constraint", "-C", type=str, default="haswell", help="Constraint for qproc batch job")
