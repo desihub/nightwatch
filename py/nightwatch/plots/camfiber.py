Basic colorbar scaling fix and colorbar scaling by camera added

py/nightwatch/plots/camfiber.py
@@ -7,6 +7,7 @@ import bokeh
 import bokeh.plotting as bk
 import bokeh.palettes as bp
 from bokeh.models import TapTool, OpenURL
+from bokeh.models import ColumnDataSource, CDSView, BooleanFilter
 
 from ..plots.fiber import plot_fibers_focalplane, plot_fibernums
 # from ..plots.core import get_colors
@@ -39,10 +40,10 @@ def plot_camfib_focalplane(cds, attribute, cameras, percentiles={},
 
     #- adjusts for outliers on the full scale
     #- change back to (2.5, 97.5) for the middle 95% for real data...?
-    pmin, pmax = np.percentile(metric, (0, 95))
+    #pmin, pmax = np.percentile(metric, (2.5, 97.5))
 
     #- common scale for all histograms for this metric
-    hist_x_range = (pmin * 0.99, pmax * 1.01)
+    #hist_x_range = (pmin * 0.99, pmax * 1.01)
 
     #- for hover tool
     attr_formatted_str = "@" + attribute + '{(0.00 a)}'
@@ -66,10 +67,20 @@ def plot_camfib_focalplane(cds, attribute, cameras, percentiles={},
             fig_x_range = figs_list[0].x_range
             fig_y_range = figs_list[0].y_range
 
-        if i == (len(cameras) - 1):
-            colorbar = True
-        else:
-            colorbar = False
+        #if i == (len(cameras) - 1):
+        #    colorbar = True
+        #else:
+        #    colorbar = False
+
+        booleans_metric = np.char.upper(np.array(cds.data['CAM']).astype(str)) == c.upper()
+        cam_metric = metric[booleans_metric]
+
+
+        pmin, pmax = np.percentile(cam_metric, (2.5, 97.5))
+
+        colorbar = True
+
+        hist_x_range = (pmin * 0.99, pmax * 1.01)
 
         fig, hfig = plot_fibers_focalplane(cds, attribute, cam=c,
                         percentile=percentiles.get(c),
@@ -112,7 +123,7 @@ def plot_camfib_fot(cds, attribute, cameras, percentiles={},
 
     #- adjusts for outliers on the full scale
     #- change back to (2.5, 97.5) for the middle 95% for real data...?
-    pmin, pmax = np.percentile(metric, (0, 95))
+    pmin, pmax = np.percentile(metric, (2.5, 97.5))
 
     #- common scale for all histograms for this metric
     hist_x_range = (pmin * 0.99, pmax * 1.01)
@@ -138,6 +149,16 @@ def plot_camfib_fot(cds, attribute, cameras, percentiles={},
             fig_x_range = figs_list[0].x_range
             fig_y_range = figs_list[0].y_range
 
+        booleans_metric = np.char.upper(np.array(cds.data['CAM']).astype(str)) == c.upper()
+        cam_metric = metric[booleans_metric]
+
+
+        pmin, pmax = np.percentile(cam_metric, (2.5, 97.5))
+
+        colorbar = True
+
+        hist_x_range = (pmin * 0.99, pmax * 1.01)
+
         fig, hfig = plot_fibers_focalplane(cds, attribute, cam=c,
                         percentile=percentiles.get(c),
                         zmin=zmins.get(c), zmax=zmaxs.get(c),
@@ -159,7 +180,7 @@ def plot_camfib_posacc(pcd,attribute,percentiles={},
     hfigs_list = []
     metric = np.array(pcd.data.get(attribute), copy=True)
     metric = metric[np.where(metric>0)] #removed nan values
-    pmin, pmax = np.percentile(metric, (0, 95))
+    pmin, pmax = np.percentile(metric, (2.5, 97.5))
     #hist_x_range = (pmin * 0.99, pmax * 1.01)
 
     if attribute == 'BLIND':
@@ -224,7 +245,7 @@ def plot_per_fibernum(cds, attribute, cameras, titles={},
 
     #- adjusts for outliers on the full scale
     #- change back to (2.5, 97.5) for the middle 95% for real data...?
-    pmin, pmax = np.percentile(metric, (0, 95))
+    pmin, pmax = np.percentile(metric, (2.5, 97.5))
 
     #- for hover tool
     attr_formatted_str = "@" + attribute + '{(0.00 a)}'

py/nightwatch/plots/fiber.py
@@ -1,7 +1,7 @@
 import numpy as np
 import jinja2
 import desimodel.io
-
+import scipy.stats
 import bokeh
 import bokeh.plotting as bk
 import bokeh.models
@@ -52,8 +52,12 @@ def plot_fibers_focalplane(source, name, cam='',
     '''
     full_metric =np.array(source.data.get(name), copy=True)
 
-    #- adjusts for outliers on the full scale
-    pmin_full, pmax_full = np.percentile(full_metric, (0, 95))
+    #- adjusts for outliers on each camera's scale
+
+    in_cam = np.char.upper(np.array(source.data['CAM']).astype(str)) == cam.upper()
+    cam_metric = full_metric[in_cam]
+
+    pmin_full, pmax_full = np.percentile(cam_metric, (2.5, 97.5))
 
     #- Generate colors for both plots
     if not palette:
@@ -135,9 +139,6 @@ def plot_fibers_focalplane(source, name, cam='',
     if not plot_hist:
         return fig, None
 
-    if not plot_hist:
-        return fig, None
-
     #- Histogram of values
     metric = full_metric[booleans_metric]
 
@@ -181,7 +182,7 @@ def plot_fibernums(source, name, cam='',
     #- Will that break when plotting fiber plots individually?
     full_metric = np.array(source.data.get(name), copy=True)
     #- adjusts for outliers on the full scale
-    pmin_full, pmax_full = np.percentile(full_metric, (0, 95))
+    pmin_full, pmax_full = np.percentile(full_metric, (2.5, 97.5))
 
     #- Fibernum scatter plot
     fig = bk.figure(width=width, height=height, title=title, tools=tools,