e1c122452311f2491c627b16d81979a7f344ef4f
Benji Kan <42553807+BenjiKan@users.noreply.github.com>
2021-01-29 11:59:48 -0600

    refactor calcnominalnoise.py to write json as function, add input calibration ZEROS text file

--- py/nightwatch/threshold_files/calcnominalnoise.py
+++ py/nightwatch/threshold_files/calcnominalnoise.py
@@ -1,9 +1,11 @@
 # calculates nominal read noise for zero and dark
 # outputs FITS with all fields to [outdir]
 
-import fitsio
 import numpy as np
 import os
+import json 
+
+from ..thresholds import get_outdir
 
 def calcnominalnoise(nightwatchdir, nightexpids, outfile):
     """
@@ -14,11 +16,16 @@ def calcnominalnoise(nightwatchdir, nightexpids, outfile):
         nightexpids: list of (night,expid) tuples to load
         outfile: output JSON thresholds file to write
 
+    Output:
+        writes two READNOISE-*.json files containing the nominal (median) thresholds per amp,
+        calibrated on zeros and darks respectively, to the nightwatch/threshold_files directory
+
     TODO:
       * use all nightexpids instead of splitting by ZEROs vs. DARKs with
         hardcoded requirement that the DARK is always 2 expids after the input ZERO
-      * directly write a READNOISE-*.json file instead of an intermediate FITS file
     """
+
+    # median vs. min
 
     #---- EXAMPLE code for using function inputs
     amps = []
@@ -31,105 +38,84 @@ def calcnominalnoise(nightwatchdir, nightexpids, outfile):
         amps.append(amp)
         readnoise.append(amp["READNOISE"])
 
-    #- then do something to convert to nominal min/max values to use for thresholds
-    #- ...
+    # extract (sorted) readnoises for ZEROs and DARKs
+    zero_readnoise = []
+    dark_readnoise = []
+    for night, expid in nightexpids:
+        zerodir = os.path.join(nightwatchdir, str(night), '{:08d}'.format(int(expid)))
+        zerofile = os.path.join(zerodir, 'qa-{:08d}.fits'.format(int(expid)))
+        zeroamp = fitsio.read(zerofile, "PER_AMP")
+        zeroamp.sort(order=["CAM", "SPECTRO", "AMP"]) # https://numpy.org/doc/stable/reference/generated/numpy.recarray.sort.html
+        zero_readnoise.append(zeroamp["READNOISE"])
 
-    #----- end EXAMPLE
+        # requires DARK is always 2 expids after input ZERO (last ZERO of the night)
+        darkdir = os.path.join(nightwatchdir, str(night), '{:08d}'.format(int(expid)+2))
+        darkfile = os.path.join(darkdir, 'qa-{:08d}.fits'.format(int(expid)+2))
+        darkamp = fitsio.read(darkfile, "PER_AMP")
+        darkamp.sort(order=["CAM", "SPECTRO", "AMP"]) # https://numpy.org/doc/stable/reference/generated/numpy.recarray.sort.html
+        dark_readnoise.append(darkamp["READNOISE"])        
 
-    #-----
-    #- previous code, to be updated as needed
+    # calculate nominal values
+    zero_noms = np.median(zero_readnoise, axis=0)
+    dark_noms = np.median(dark_readnoise, axis=0)
+    # if nominal is minimum noise: 
+    # zeros_nom = np.amin(zeros_readnoise, axis=0)
+    # darks_nom = np.amin(darks_readnoise, axis=0)
 
-    # make darkdirs
-    darkdirs = []
-    for zerodir in zerodirs:
-        fulldir = os.path.join(nwdir, zerodir)
-        expid = os.path.basename(fulldir)
-        night = os.path.basename(os.path.dirname(fulldir))
+    # create thresholds files
+    zero_thresholds = dict()
+    dark_thresholds = dict()
+    all_amps = [cam+spec+amp for cam in ['B', 'R', 'Z'] for spec in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] for amp in ['A', 'B', 'C', 'D']]
+
+    for i, amp in enumerate(all_amps):
+        zero_ampnom = zero_noms[i]
+        zero_thresholds[amp] = dict(upper_err=zero_ampnom+1, upper=zero_ampnom+0.5,
+                                    lower=zero_ampnom-0.5, lower_err=zero_ampnom-1)
+        dark_ampnom = dark_noms[i]
+        dark_thresholds[amp] = dict(upper_err=dark_ampnom+1, upper=dark_ampnom+0.5,
+                                    lower=dark_ampnom-0.5, lower_err=dark_ampnom-1)
+
+    # write files
+    outdir = get_outdir()
+    split_outfile = outfile.split(".")
     
-        darkdir = os.path.join(night, '{:08d}'.format(int(expid)+2))
-        darkdirs.append(darkdir)
+    zero_outfile = split_outfile[0] + '-ZERO.' + split_outfile[1]
+    zero_threshold_file = os.path.join(outdir, zero_outfile)
+    with open(zero_threshold_file, 'w') as json_file:
+             json.dump(zero_thresholds, json_file, indent=4)
+    print('Wrote {}'.format(zero_threshold_file))
 
-    # create filepaths
-    zeroqafiles = []
-    for zerodir in zerodirs:
-        fulldir = os.path.join(nwdir, zerodir)
-        expid = os.path.basename(fulldir)
-        night = os.path.basename(os.path.dirname(fulldir))
-    
-        qafile = os.path.join(fulldir, 'qa-{:08d}.fits'.format(int(expid)))
-        zeroqafiles.append(qafile)
-
-    darkqafiles = []
-    for darkdir in darkdirs:
-        fulldir = os.path.join(nwdir, darkdir)
-        expid = os.path.basename(fulldir)
-        night = os.path.basename(os.path.dirname(fulldir))
-    
-        qafile = os.path.join(fulldir, 'qa-{:08d}.fits'.format(int(expid)))
-        darkqafiles.append(qafile)
-
-    # extract and sort amp files 
-    zeroamps = []
-    zeroamps_rn = []
-    for qafile in zeroqafiles:
-        data = fitsio.FITS(qafile)
-        amp = data["PER_AMP"].read()
-        amp.sort(order=["CAM", "SPECTRO", "AMP"]) # https://numpy.org/doc/stable/reference/generated/numpy.recarray.sort.html
-        zeroamps.append(amp)
-        zeroamps_rn.append(amp["READNOISE"])
-    
-    darkamps = []
-    darkamps_rn = []
-    for qafile in darkqafiles:
-        data = fitsio.FITS(qafile)
-        amp = data["PER_AMP"].read()
-        amp.sort(order=["CAM", "SPECTRO", "AMP"]) # https://numpy.org/doc/stable/reference/generated/numpy.recarray.sort.html
-        darkamps.append(amp)
-        darkamps_rn.append(amp["READNOISE"])
-    
-    # combbined rnnoms
-    nrows = 10*3*4
-    rnnoms = np.empty(nrows, dtype=[('CAM', 'U1'), ('SPECTRO', '>i8'), ('AMP', 'U1'),
-                                    ('READNOISE_NOM_ZERO','>f8'), ('READNOISE_MIN_ZERO','>f8'), ('READNOISE_STD_ZERO','>f8'), ('READNOISE_WARNING_ZERO','>i8'),
-                                    ('READNOISE_NOM_DARK','>f8'), ('READNOISE_MIN_DARK','>f8'), ('READNOISE_STD_DARK','>f8'), ('READNOISE_WARNING_DARK','>i8')])
-    rnnoms['READNOISE_NOM_ZERO'] = np.median(zeroamps_rn, axis=0)
-    rnnoms['READNOISE_MIN_ZERO'] = np.amin(zeroamps_rn, axis=0)
-    rnnoms['READNOISE_STD_ZERO'] = np.std(zeroamps_rn, axis=0)
-    rnnoms['READNOISE_WARNING_ZERO'] = np.ones(nrows)*(-1) # default to -1
-    rnnoms['READNOISE_NOM_DARK'] = np.median(darkamps_rn, axis=0)
-    rnnoms['READNOISE_MIN_DARK'] = np.amin(darkamps_rn, axis=0)
-    rnnoms['READNOISE_STD_DARK'] = np.std(darkamps_rn, axis=0)
-    rnnoms['READNOISE_WARNING_DARK'] = np.ones(nrows)
-
-    rnnoms['CAM'] = amp['CAM'] # using a random amp to get sorted order
-    rnnoms['SPECTRO'] = amp['SPECTRO']
-    rnnoms['AMP'] = amp['AMP']
-
-    fitsio.write(outfile, rnnoms)
+    dark_outfile = split_outfile[0] + '-DARK.' + split_outfile[1]
+    dark_threshold_file = os.path.join(outdir, dark_outfile)
+    with open(dark_threshold_file, 'w') as json_file:
+             json.dump(dark_thresholds, json_file, indent=4)
+    print('Wrote {}'.format(dark_threshold_file))
+        
     
 if __name__ == "__main__":
     """
     e.g.
     python calcnominalreadnoise.py --indir /global/cfs/cdirs/desi/spectro/nightwatch/nersc \
-        --nightexpids 20210107/00071175 20210108/00071353 20210109/00071521 20210110/00071664 20210111/00071802 \
-        --outfile READNOISE-20210107.json
+        --nightexpids /global/cfs/cdirs/desi/spectro/nightwatch/nersc/py/nightwatch/threshold_files/ZEROS-20210111.txt \
+        --outfile READNOISE-20210111.json
     """
     import argparse
 
     parser = argparse.ArgumentParser(usage = "{prog} [options]")
     parser.add_argument("--indir", type=str,  help="base directory with nightwatch processed data")
-    parser.add_argument("--nightexpids", type=str, nargs="*", help="input night/expids to use for readnoise")
+    parser.add_argument("--nightexpids", type=str, nargs="*", help="text file containing input night/expids (last ZERO of the night) to use for readnoise")
     parser.add_argument("--outfile", type=str,  help="output json threshold file to write")
     args = parser.parse_args()
 
-    #- TODO: it might be easier for --nightexpids to be a text file that contains
-    #- the night/expids to use, one per line, instead of trying to specify N>>1
-    #- night/expids from a command line
-
+    f = open(args.nightexpids, "r")
+    inputids = f.read()
+    f.close()
+    
     #- convert input nightexpids strings to list of (night,expid) tuples
     nightexpids = list()
-    for tmp in args.nightexpids:
+    for tmp in inputids:
         n, e = tmp.split('/')
-        nightexpids = int(n), int(e)
+        nightexpid = (int(n), int(e))
+        nightexpids.append(nightexpid)
 
     calcnominalnoise(args.indir, nightexpids, args.outfile)
