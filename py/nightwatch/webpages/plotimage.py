137fa43e2cbd817ba0453a244f1e342df2854a9b
Benji Kan <42553807+BenjiKan@users.noreply.github.com>
2021-03-25 00:17:53 -0500

    make and add composite preproc plots

--- py/nightwatch/webpages/plotimage.py
+++ py/nightwatch/webpages/plotimage.py
@@ -81,6 +81,15 @@ def write_preproc_table_html(input_dir, night, expid, downsample, output):
         current=None, expid=int(expid), zexpid='{:08d}'.format(expid),
         num_dirs=2, qatype='amp',
     )
+    
+    make_composites(input_dir, night, expid)
+    preproc_composites = [i for i in os.listdir(input_dir) if re.match(r'preproc.*composite*', i)]
+
+    cams = ["Bcomposite", "Rcomposite", "Zcomposite"]
+    for cam, composite in zip(cams, preproc_composites):
+        compositepath = os.join(input_dir, composite)
+        script, div = main(compositepath, None, downsample)
+        html_components[cam] = dict(script=script, div=div)
 
     html = template.render(**html_components)
 
@@ -89,3 +98,78 @@ def write_preproc_table_html(input_dir, night, expid, downsample, output):
         fx.write(html)
 
     print('Wrote {}'.format(output))
+
+
+
+import numpy as np
+from glob import glob
+import fitsio
+from astropy.io import fits
+
+def load_preproc_portion(preproc):
+    '''
+    Splices together 20 vertical strips, 2 from each preproc files
+    Inputs:
+        preproc: list of ordered preproc files for a given camera
+
+    '''
+    size = 200
+    xmin = 0
+    xmax = 4000
+    #xmin = 1000
+    #xmax = 3000
+    images = []
+    restart = 2048
+    start = restart-len(preproc)*size
+    for n, ppfile in enumerate(preproc):
+        file = fitsio.FITS(ppfile) ## fix this?
+        images.append(file["IMAGE"][xmin:xmax,start+n*size:start+(n+1)*size])
+        images.append(file["IMAGE"][xmin:xmax,restart+n*size:restart+(n+1)*size])
+        #print(ppfile, np.shape(images[-1]))
+    
+    full = np.empty([np.shape(images[0])[0], size*len(images)], dtype=np.float32)
+    #print(np.shape(full))
+    slot = np.arange(len(images))
+    slot = (slot//2 + (slot%2)*len(images)/2).astype(int32)
+    for n, im in enumerate(images):
+        full[:,slot[n]*size:(slot[n]+1)*size] = images[n]
+    extent = [start, start+size*20, xmin, xmax]
+    return full, extent
+
+def make_composites(preprocdir, night, expid):
+    '''
+    Compiles composite preproc images for each camera and writes .fits file to preprocdir
+    Inputs:
+        preprocdir: directory containing subdirectories with preproc images
+        night: YYYYMMDD
+        expid: exposure preproc images belong to
+
+    '''
+    preprocfiles = sorted(glob(preprocdir+'/preproc*.fits'))
+    if len(preprocfiles)<30: 
+        print("Couldn't find the preproc spectroscopy files in", directory)
+        return
+    roll = -(expid%10)
+    b_image = load_preproc_portion(np.roll(preprocfiles[0:10],roll))
+    r_image = load_preproc_portion(np.roll(preprocfiles[10:20],roll))
+    z_image = load_preproc_portion(np.roll(preprocfiles[20:30],roll))
+    print("preprocs loaded")
+        
+    hdu = fits.PrimaryHDU(b_image[0])
+    outfile =  os.path.join(preprocdir, "preproc-{}-{:08d}.fits").format("b" + "composite", expid)
+    hdu.writeto(outfile, overwrite=True)
+    print("b composite done")
+    
+    hdu = fits.PrimaryHDU(r_image[0])
+    outfile =  os.path.join(preprocdir, "preproc-{}-{:08d}.fits").format("r" + "composite", expid)
+    hdu.writeto(outfile, overwrite=True)
+    print("r composite done")
+
+    hdu = fits.PrimaryHDU(z_image[0])
+    outfile =  os.path.join(preprocdir, "preproc-{}-{:08d}.fits").format("z" + "composite", expid)
+    hdu.writeto(outfile, overwrite=True)
+    print("z composite done")
+
+def sqrt_stretch(x):
+    return x/np.sqrt(np.abs(x+1e-10))
+
