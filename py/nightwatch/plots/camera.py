2eda8ad3651465b602f520ddb237c74e2d74cf24
Segev BenZvi <sybenzvi@gmail.com>
2022-01-14 18:43:52 -0800

    Hack in visible warning/error markers for cam QA.

--- py/nightwatch/plots/camera.py
+++ py/nightwatch/plots/camera.py
@@ -5,6 +5,61 @@ from bokeh.layouts import column
 from bokeh.models.tickers import FixedTicker
 from astropy.table import Table
 
+
+def get_qa_colors(data, lower_err, lower, upper, upper_err, basecolor='black'):
+    '''takes in camera QA data and the acceptable threshold for that metric.
+    Args: 
+        data: array of QA data 
+        lower_err: array of lower thresholds that trigger errors
+        lower: array of lower thresholds that trigger warnings
+        upper: array of upper thresholds that trigger warnings
+        upper_err: array of upper thresholds that trigger errors
+    Output: array of colors to be put into a ColumnDataSource
+    '''
+    colors = []
+    for i in range(len(data)):
+        if lower == None or upper == None:
+            continue
+        if data[i] <= lower_err:
+            colors.append('red')
+        if data[i] > lower_err and data[i] <= lower:
+            colors.append('orange')
+        if data[i] > lower and data[i] < upper:
+            colors.append(basecolor)
+        if data[i] >= upper and data[i] < upper_err:
+            colors.append('orange')
+        if data[i] >= upper_err:
+            colors.append('red')
+    return colors
+
+
+def get_qa_size(data, lower_err, lower, upper, upper_err):
+    '''takes in camera QA data and the acceptable threshold for that metric
+    Args: 
+        data: array of QA data
+        lower_err: array of lower thresholds that trigger errors
+        lower: array of lower thresholds that trigger warnings
+        upper: array of upper thresholds that trigger warnings
+        upper_err: array of upper thresholds that trigger errors
+    Output: array of sizes for markers to be put into a ColumnDataSource
+    '''
+    sizes = []
+    for i in range(len(data)):
+        if lower == None or upper == None:
+            continue
+        if data[i] <= lower_err:
+            sizes.append('9')
+        if data[i] > lower_err and data[i] <= lower:
+            sizes.append('9')
+        if data[i] > lower and data[i] < upper:
+            sizes.append('4')
+        if data[i] >= upper and data[i] < upper_err:
+            sizes.append('9')
+        if data[i] >= upper_err:
+            sizes.append('9')
+    return sizes
+
+
 def plot_camera_qa(table, attribute, unit=None, lower=None, upper=None, height=225, width=450, title=None, line0 = True, minmax=None):
     '''
     Creates 3 plots of an attribute vs Spectrograph number, corresponding to
@@ -35,9 +90,9 @@ def plot_camera_qa(table, attribute, unit=None, lower=None, upper=None, height=2
 
     cam_figs=[]
 
-    colors = {"B":"blue", "R":"red", "Z":"green"}
+    camcolors = {"B":"blue", "R":"red", "Z":"green"}
     keys = {'B':0, 'R':1, 'Z':2}
-    for cam in ["B", "R", "Z"]:
+    for cam in 'BRZ':
         
         cam_table = astrotable[astrotable["CAM"]==cam]
 
@@ -47,13 +102,36 @@ def plot_camera_qa(table, attribute, unit=None, lower=None, upper=None, height=2
             continue
 
         fig = bk.figure(plot_height=height, plot_width=width, title = title+" "+cam, tools=['reset', 'box_zoom', 'pan'])
-        source = ColumnDataSource(data=dict(
-            SPECTRO = cam_table["SPECTRO"],
-            MEANattr = cam_table["MEAN"+attribute],
-            MINattr = cam_table["MIN"+attribute],
-            MAXattr = cam_table["MAX"+attribute]
-        ))
-        fig.circle(source=source, x="SPECTRO", y="MEANattr", color=colors[cam])
+
+        if attribute == 'DX' or attribute == 'DY':
+            k = keys[cam]
+            lower_err  = lower[k][0][0] + lower[k][1][0]
+            lower_warn = lower[k][1][0]
+            upper_warn = upper[k][0][0]
+            upper_err  = upper[k][0][0] + upper[k][1][0]
+
+            colors = get_qa_colors(cam_table['MEAN'+attribute], lower_err, lower_warn, upper_warn, upper_err, basecolor=camcolors[cam])
+            sizes = get_qa_size(cam_table['MEAN'+attribute], lower_err, lower_warn, upper_warn, upper_err)
+
+            source = ColumnDataSource(data=dict(
+                SPECTRO = cam_table["SPECTRO"],
+                MEANattr = cam_table["MEAN"+attribute],
+                MINattr = cam_table["MIN"+attribute],
+                MAXattr = cam_table["MAX"+attribute],
+                colors = colors,
+                sizes = sizes
+            ))
+
+            fig.circle(source=source, x="SPECTRO", y="MEANattr", color='colors', size='sizes')
+        else:
+            source = ColumnDataSource(data=dict(
+                SPECTRO = cam_table["SPECTRO"],
+                MEANattr = cam_table["MEAN"+attribute],
+                MINattr = cam_table["MIN"+attribute],
+                MAXattr = cam_table["MAX"+attribute]
+            ))
+            fig.circle(source=source, x="SPECTRO", y="MEANattr", color=camcolors[cam])
+
         fig.circle(source=source, x="SPECTRO", y="MAXattr", fill_alpha=0, line_alpha=0)
         fig.circle(source=source, x="SPECTRO", y="MINattr", fill_alpha=0, line_alpha=0)
         if line0:
