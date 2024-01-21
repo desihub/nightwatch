ddc8b538a111391755401e48a7bfdcce494cef5a
Segev BenZvi <sybenzvi@gmail.com>
2024-01-21 11:47:35 -0500

    Put sphinx docs into a separate folder.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -18,7 +18,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Nightwatch'
-copyright = '2024, DESI Collaboration'
+copyright = '2019-2024, DESI Collaboration'
 author = 'DESI Collaboration'
 
 
@@ -44,7 +44,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'default'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
