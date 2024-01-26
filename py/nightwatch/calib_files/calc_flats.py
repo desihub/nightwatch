2e167bd79d75fd2d6d7999dd14e53438552b9d4b
Segev BenZvi <sybenzvi@users.noreply.github.com>
2024-01-26 17:02:18 -0500

    Revert calc_flats.py

--- py/nightwatch/calib_files/calc_flats.py
+++ py/nightwatch/calib_files/calc_flats.py
@@ -108,8 +108,6 @@ def calc_flats(datadir, night, expids, prog, warnlevel=0.01, errlevel=0.02):
 
 
 if __name__ == '__main__':
-    print('\n\033[91m ** WARNING ** This program is deprecated; use gen_flat_refs.py\n\033[0m')
-
     p = ArgumentParser(usage='{prog} [options]',
                        formatter_class=ArgumentDefaultsHelpFormatter)
     p.add_argument('-n', '--nightexpids', type=str,
