758fe3d42c7834404009f95a902f63cda9b807d1
Segev BenZvi <sybenzvi@gmail.com>
2024-01-26 22:41:11 -0500

    Set up external doc dependencies.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -28,6 +28,8 @@ author = 'DESI Collaboration'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_rtd_theme',
+    'sphinx_search.extension',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -44,7 +46,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
+
+html_favicon = 'img/nw-icon.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
